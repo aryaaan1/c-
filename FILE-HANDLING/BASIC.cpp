30327f8a4fbf14b3cd69fa533b206004ad747af7
ARYAN <whyonlyaryan1554@gmail.com>
2026-09-14 13:57:23 +0530

    added file handling

---
+++ FILE-HANDLING/BASIC.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main (){
+    ofstream fout;
+    fout.open("zoom.txt");
+    fout<<"hello world may name is aryan what is your name ";
+    fout.close();
+}
+
+
+
--- STANDARD-TEMPLATE-LIBRARY/4-FUNCTORS/2-CUSTOM-FUNCTORS/3-CUSTOM-PRIORITY-QUEUE.cpp
+++ STANDARD-TEMPLATE-LIBRARY/4-FUNCTORS/2-CUSTOM-FUNCTORS/3-CUSTOM-PRIORITY-QUEUE.cpp
@@ -1,4 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<queue>
 using namespace std;
+int main (){
+
+    priority_queue<int,vector<int>,greater<int>>mylist;
+    mylist.push(12);
+    mylist.push(13);
+    mylist.push(123);
+    mylist.push(126);
+
+    
+   
+
+    
+
+}
